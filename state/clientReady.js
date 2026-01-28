5e0aebae30a714a0cf3dc4ea35be9b7847b977fe
OtBbNA <tosha.kostylev.97@mail.ru>
2026-01-28 21:12:13 +0300

    BugFix #46o upd: clientReady.js

--- state/clientReady.js
+++ state/clientReady.js
@@ -2,13 +2,8 @@ import { client } from './discordClient.js';
 
 export let clientReady = false;
 export const setClientReady = () => (clientReady = true);
-export const clientReady = new Promise((resolve) => {
-    if (client.isReady?.()) {
-        resolve();
-    } else {
-        client.once('ready', () => {
-            console.log(`🤖 Logged in as ${client.user.tag}`);
-            setClientReady();
-        });
-    }
+
+client.once('ready', () => {
+    console.log(`🤖 Logged in as ${client.user.tag}`);
+    setClientReady();
 });
