83c2752eca6d36f2468ba5312e91e32f7d15e89b
OtBbNA <tosha.kostylev.97@mail.ru>
2026-01-29 21:53:21 +0300

    Update b #19c: bugfix - add client into app.js

--- riceBot/app.js
+++ riceBot/app.js
@@ -2,18 +2,19 @@ import express from 'express';
 import { verifyKeyMiddleware } from 'discord-interactions';
 import { config } from './config.js';
 import { handleInteraction } from './interactions/index.js';
-import './client.js';
+import { client } from './client.js';
 
 const app = express();
 
-app.post(
+app.use(
     '/interactions',
-    express.raw({ type: '*/*' }),
     verifyKeyMiddleware(config.publicKey),
-    handleInteraction
+    express.json(),
+    (req, res) => {
+        req.client = client;
+        handleInteraction(req, res);
+    }
 );
-
-app.get('/ping', (_, res) => res.send('ok'));
 
 app.listen(config.port, () =>
 console.log(`🚀 Server running on port ${config.port}`)
